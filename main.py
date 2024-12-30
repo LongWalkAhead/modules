0f426f98ecfba11cbedfd9ee21d53f147ee31bbe
LongWalkAhead <bluelama1234@gmail.com>
2024-12-30 11:04:55 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+import mood_responses
+import text_utils as tu
+mood = input("How are you feeling today? ")
+print(mood_responses.mood_response(mood))
+print(tu.reverse_string("hello"))
+print(tu.capitalize_string("goodbye"))
---
+++ mood_responses.py
@@ -0,0 +1,12 @@
+
+
+def mood_response(mood):
+     return f"today you were feeling {mood}"
+          
+
+   
+
+      
+
+
+
